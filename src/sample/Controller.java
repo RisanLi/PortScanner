41d3436bb0b09d0effd7b428da6681fc264ed6ec
Netzhangheng <Netzhangheng@gmail.com>
2018-09-12 16:10:03 +0800

    Design PortScanner GUI

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,38 @@
 package sample;
 
+import javafx.scene.control.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Netzhang
+ * Description:
+ * Date: 2018-09-12
+ * Time: 9:56
+ */
 public class Controller {
+    public MenuBar menuBar;
+        public Menu menu_menu;
+            public MenuItem menuItem_startScan;
+            public MenuItem menuItem_export;
+            public MenuItem menuItem_exit;
+        public Menu menu_about;
+            public MenuItem menuItem_author;
+            public MenuItem menuItem_contect;
+            public MenuItem menuItem_version;
+            public MenuItem menuItem_help;
+
+    public TextField textField_IP;
+    public TextField textField_thread;
+
+    public ToggleGroup scanChoose;
+        public RadioButton radioButton_commonPort;
+            public TextField textField_commonPort;
+        public RadioButton radioButton_continuousPort;
+            public TextField textField_startPort;
+            public TextField textField_endPort;
+
+    public Button button_start;
+    public Button button_export;
+    public TextArea textArea_scanProcess;
+    public TextArea textArea_result;
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,11 +11,11 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Scene scene = new Scene(root);
+
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
